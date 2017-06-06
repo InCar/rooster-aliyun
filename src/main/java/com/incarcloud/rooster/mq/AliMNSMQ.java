153b20d779bd3c1dc92e8179aa6c8b5cef213563
youbeiwuhuan <bbfan@incarcloud.com>
2017-06-06 14:32:16 +0800

    从rooster-shared移动mns

--- src/main/java/com/incarcloud/rooster/mq/AliMNSMQ.java
+++ src/main/java/com/incarcloud/rooster/mq/AliMNSMQ.java
@@ -1,12 +1,59 @@
 package com.incarcloud.rooster.mq;
 
-import org.jdeferred.Promise;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
+
+import com.aliyun.mns.common.ClientException;
+import com.aliyun.mns.common.ServiceException;
+import com.incarcloud.rooster.aliyun.MnsClient;
+
 public class AliMNSMQ implements IBigMQ {
-    @Override
-    public Promise<Object, List<MQException>, Object> post(List<MQMsg> listMsgs) {
-        return null;
-    }
+	private static org.slf4j.Logger s_logger = LoggerFactory.getLogger(AliMNSMQ.class);
+
+	private MnsClient mnsClient;
+
+	/**
+	 * @param mnsClient
+	 */
+	public AliMNSMQ(MnsClient mnsClient) {
+		this.mnsClient = mnsClient;
+	}
+
+	@Override
+	public List<MqSendResult> post(List<MQMsg> listMsgs) {
+
+		if (null == listMsgs || 0 == listMsgs.size()) {
+			throw new IllegalArgumentException("message list is null");
+		}
+
+		List<MqSendResult> resultList = new ArrayList<>(listMsgs.size());
+
+		for (MQMsg msg : listMsgs) {
+
+			try {
+				String msgId = mnsClient.sendMessage("rooster-dev", msg.getData());
+				resultList.add(new MqSendResult(null, msgId));
+			} catch (ClientException ce) {
+
+				resultList.add(new MqSendResult(
+						new MQException("Something wrong with the network connection between client and MNS service."
+								+ "Please check your network and DNS availablity."),
+						null));
+//				ce.printStackTrace();
+				s_logger.debug("58",ce.getMessage());
+			} catch (ServiceException se) {
+				resultList.add(new MqSendResult(new MQException(se.getErrorCode()), null));
+//				se.printStackTrace();
+				s_logger.debug("62",se.getMessage());
+			} catch (Exception e) {
+				resultList.add(new MqSendResult(new MQException(e.getMessage()), null));
+//				e.printStackTrace();
+				s_logger.debug(e.getMessage());
+			}
+		}
+
+		return resultList;
+	}
 }
