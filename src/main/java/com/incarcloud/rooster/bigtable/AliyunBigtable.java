5e3b5b593f59d4415219c7a5ae1f937bd2f68144
youbeiwuhuan <fan794919764@126.com>
2017-07-25 10:57:12 +0800

    DataTarget -> DataPackObject

--- src/main/java/com/incarcloud/rooster/bigtable/AliyunBigtable.java
+++ src/main/java/com/incarcloud/rooster/bigtable/AliyunBigtable.java
@@ -2,7 +2,7 @@ package com.incarcloud.rooster.bigtable;/**
  * Created by fanbeibei on 2017/7/5.
  */
 
-import com.incarcloud.rooster.datatarget.DataTarget;
+import com.incarcloud.rooster.datapack.DataPackObject;
 import com.incarcloud.rooster.util.DataTargetUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +20,7 @@ public class AliyunBigtable implements IBigTable {
         this.client = client;
     }
 
-    public void save(String rowKey, DataTarget data, String tableName) throws Exception {
+    public void save(String rowKey, DataPackObject data, String tableName) throws Exception {
 
         client.insert(rowKey, DataTargetUtils.toJson(data),tableName);
         s_logger.debug("save to tablestore success:"+rowKey);
